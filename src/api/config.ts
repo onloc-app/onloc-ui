c2de83053bdb8e5c4d52af7e49ceacf5491df6d2
calvicii <lavoiethomas17@gmail.com>
2026-01-11 18:42:03 -0500

    Change wrong default port

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL = `${window.location.protocol}//${
   window.location.hostname
-}:${import.meta.env.VITE_WS_PORT || "6145"}`
+}:${import.meta.env.VITE_WS_PORT || "6144"}`
 export const API_URL = `${SERVER_URL}/api`
