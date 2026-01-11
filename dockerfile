e340efa6f1f7a362842efbc7875f4899de5ed791
calvicii <lavoiethomas17@gmail.com>
2026-01-11 18:26:18 -0500

    Add nginx and build files

--- dockerfile
+++ dockerfile
@@ -3,13 +3,17 @@ FROM oven/bun:latest AS base
 WORKDIR /app
 
 COPY package.json bun.lock ./
-
 RUN bun install
 
 COPY . .
-
 RUN bun run build
+
+FROM nginx:alpine AS prod
+
+RUN rm -rf /usr/share/nginx/html/*
+COPY --from=base /app/dist /usr/share/nginx/html
+COPY ./nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 3000
 
-CMD ["bun", "run", "dev"]
+CMD ["nginx", "-g", "daemon off"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv, type UserConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }): UserConfig => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  let allowedHosts: true | string[] | undefined
+
+  if (env.VITE_ALLOWED_HOSTS === "all") {
+    allowedHosts = true
+  } else if (env.VITE_ALLOWED_HOSTS) {
+    allowedHosts = env.VITE_ALLOWED_HOSTS.split(",")
+      .map((host) => host.trim())
+      .filter(Boolean)
+  }
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    open: true,
-  },
+    server: {
+      port: 3000,
+      strictPort: true,
+      open: false,
+      allowedHosts: allowedHosts,
+    },
+  }
 })
