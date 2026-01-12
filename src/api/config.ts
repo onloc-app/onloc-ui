6467b30c0e5c7bbf8ca8363fbc0e09396c8c7e7a
calvicii <lavoiethomas17@gmail.com>
2026-01-12 14:20:06 -0500

    Change custom backend port

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,5 @@
-export const SERVER_URL = `${window.location.protocol}//${
-  window.location.hostname
-}:${import.meta.env.VITE_WS_PORT || "6144"}`
+const port = import.meta.env.VITE_BACKEND_PORT ?? window.location.port
+
+export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}${port ? `:${port}` : ""}`
+
 export const API_URL = `${SERVER_URL}/api`
