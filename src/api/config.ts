ae2ed34475ebeb4de017eef11e2edd8f47c71583
Maximilian Mikus <maximilianmikus@gmail.com>
2026-06-29 11:53:39 +0200

    feat: make configurable server api url fallback to regular server url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 const port = import.meta.env.VITE_BACKEND_PORT ?? window.location.port
-export const API_SERVER_URL = import.meta.env.VITE_ONLOC_API_SERVER_URL
-
 export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}${port ? `:${port}` : ""}`
 
-export const API_URL = API_SERVER_URL ? `${API_SERVER_URL}/api`:  `${SERVER_URL}/api`
+export const API_SERVER_URL = import.meta.env.VITE_ONLOC_API_SERVER_URL ?? SERVER_URL
+
+export const API_URL = `${API_SERVER_URL}/api`
