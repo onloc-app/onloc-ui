365b3ce23558a9eb722f1eaf3c88d9429fb00dde
Maximilian Mikus <maximilianmikus@gmail.com>
2026-06-29 11:27:05 +0200

    feat: make the api server url configurable via env variable

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,6 @@
 const port = import.meta.env.VITE_BACKEND_PORT ?? window.location.port
+const apiServerUrl = import.meta.env.VITE_ONLOC_API_SERVER_URL
 
 export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}${port ? `:${port}` : ""}`
 
-export const API_URL = `${SERVER_URL}/api`
+export const API_URL = apiServerUrl ?? `${SERVER_URL}/api`
