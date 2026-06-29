727f71b7572da9d501305f31aeef917b71ece1bf
calvicii <lavoiethomas17@gmail.com>
2026-06-29 11:50:38 -0400

    Rename some .env variables

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-const port = import.meta.env.VITE_BACKEND_PORT ?? window.location.port
+const port = import.meta.env.VITE_ONLOC_API_PORT ?? window.location.port
 export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}${port ? `:${port}` : ""}`
 
 export const API_SERVER_URL = import.meta.env.VITE_ONLOC_API_SERVER_URL ?? SERVER_URL
